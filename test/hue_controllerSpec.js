e2abf0b9aa032378ab2c827d108a78496911303e
Dave Jensen <screamingchameleons1@gmail.com>
2016-09-02 22:48:03 -0600

    Added an array of lights, which hold the "on", "off" and "rgb" methods now.  The array of lights is returned from the hue controller's "start" method.

--- test/hue_controllerSpec.js
+++ test/hue_controllerSpec.js
@@ -5,52 +5,72 @@ let should = chai.should();
 
 describe('hue lights controller', function (){
     let controller = new (require("../lib/hue-controller"))()
-    let lights = []
+    let hue_lights = []
 
     before(function(done){     
        let startPromise = controller.start()
         .then((lights) => {
-            let val = {test: true}
-            val.test.should.exist
+            hue_lights = lights
+            hue_lights.length.should.be.above(0)
             done()
         })
     })
-
-   it('should construct and start', function() {
-       console.log("I am in the first test")
-   })
-
+/*
    it('should return config info', function(done) {
-       controller.getConfig()
-        .then((data) => {
-            console.log(JSON.stringify(data))
-            data.name.should.eql("Philips hue")
-            done()
-        })
-   })
-
-   it('should turn on a light', function(done) {
-       controller.turnOn()
-        .then(() => {
-            done()
-        })
-   })
-
-   it('should set colors on a light', function(done) {
-       controller.setRgb(255, 0, 0)
-        .then(() => {
-            setTimeout(() => {
-                controller.setRgb(0, 255, 0)
+        controller.getConfig()
+            .then((data) => {
+                console.log(JSON.stringify(data))
+                data.name.should.eql("Philips hue")
                 done()
-            }, 100)
+            })
+   })
+*/
+   it('should turn on a light', function(done) {
+       let promises = [];
+
+       for(var item of hue_lights) {
+           let p = new Promise((resolve, reject) => {
+                item.turnOn()
+                .then(() => {
+                    item.setRgb(255, 0, 0)
+                    .then(() => {
+                        resolve();
+                    })
+                    .catch((err) => {
+                        console.log(err)
+                    })
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+           })
+           promises.push(p)
+       }
+       Promise.all(promises)
+       .then(() => {
+           done()
         })
+   })
+/*
+   it('should set colors on a light', function(done) {
+       for(var item of hue_lights) {
+        item.setRgb(255, 0, 0)
+            .then(() => {
+                setTimeout(() => {
+                    item.setRgb(0, 255, 0)
+                    done()
+                }, 100)
+            })
+       }
    })
 
    it('should turn off a light', function(done) {
-       controller.turnOff()
-        .then(() => {
-            done()
-        })
+       for(var item of hue_lights) {
+        item.turnOff()
+            .then(() => {
+                done()
+            })
+       }
    })
-
+*/
 })
