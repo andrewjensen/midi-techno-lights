332e865950e81cf62918bcc9825128f875417ea5
Dave Jensen <screamingchameleons1@gmail.com>
2016-09-06 21:12:17 -0600

    querty.push now returns a promise. When that promise is resolved, it gives the same value that came back from the pushed function when it was resolved.

--- test/hue-controller.spec.js
+++ test/hue-controller.spec.js
@@ -7,69 +7,59 @@ describe('hue lights controller', function (){
     let hue_lights = []
     let querty = new (require("../lib/querty"))()
 
-    function finishTest(done) {
+    function nullFunction() {
         let p = new Promise((resolve, reject) => {
-            console.log("Test is complete")
             resolve()
-            done()
         })
         return p;
     }
 
-    before(function(done){     
+    before(function(done){
+        this.timeout(10000)
        let startPromise = controller.start()
         .then((lights) => {
             hue_lights = lights
-            console.log(hue_lights.length)
             assert(hue_lights.length > 0)
+
+            // The API isn't supposed to require this, but if you don't have a little delay,
+            // then you'll get ECONNRESET back from the API sometimes.
             setTimeout(() => {
                 done()
-            }, 4000)
+            }, 100)
         })
     })
 
    it('should return config info', function(done) {
-
-       // I really want a way to ask querty to call something before moving on to the next
-       // thing in the queue.
-        querty.push(controller.getConfig.bind(controller));
-/*
+        querty.push(controller.getConfig.bind(controller))
             .then((data) => {
-                console.log(JSON.stringify(data))
-                data.name.should.eql("Philips hue")
+                assert.equal(data.name, "Philips hue")
                 done()
             })
-*/
-        querty.push(finishTest.bind(this, done))
    })
 
    it('should turn on the lights', function(done) {
-       console.log("getting started")
        for(var item of hue_lights) {
             querty.push(item.turnOn.bind(item));
             querty.push(item.setRgb.bind(item, 255, 0, 0))
             querty.push(item.setRgb.bind(item, 0, 255, 0))
             querty.push(item.setRgb.bind(item, 0, 0, 255))
        }
-       console.log("Pushed the batch")
-        querty.push(finishTest.bind(this, done))
-       console.log("Pushed the finish event")
+        querty.push(nullFunction).then(() => { done() })
    })
 
    it.skip('should set colors on a light', function(done) {
-       console.log("Second test")
        for(var item of hue_lights) {
            querty.push(item.setRgb.bind(item, 255, 0, 0))
            querty.push(item.setRgb.bind(item, 0, 0, 255))
        }
-        querty.push(finishTest.bind(this, done))
+        querty.push(nullFunction).then(() => { done() })
    })
 
    it('should turn off a light', function(done) {
        for(var item of hue_lights) {
             querty.push(item.turnOff.bind(item));
        }
-        querty.push(finishTest.bind(this, done))
-   })
+        querty.push(nullFunction).then(() => { done() })
+ })
 
 })
--- test/querty.spec.js
+++ test/querty.spec.js
@@ -26,6 +26,17 @@ describe('querty promise-based function queueing', function (){
         return p;
     }
 
+    function testSomeDelay(delay, num) {
+        let p = new Promise((resolve, reject) => {
+
+            setTimeout((num) => {
+                resolve(num)
+            }, delay, num)
+        })
+        return p;
+    }
+
+
     it('should queue a function call', function(done) {
         querty.push(testConsecutive.bind(this, 1));
         done()
@@ -44,5 +55,25 @@ describe('querty promise-based function queueing', function (){
         }
         querty.push(checkResults.bind(this, limit, done))
     })
+
+    it('should wait for a promise from each function call', function(done) {
+
+        // Pass in a value to be consumed in the bound function (the delay)
+        // and also a value that should be returned back after the delay through the promise chain
+        // Make sure the delay was at least the amount specified
+        // And make sure the value came back.
+        let val = 376892
+        let delay = 250
+        let start = new Date()
+        querty.push(testSomeDelay.bind(this, 250, val))
+            .then((data) => {
+                let end = new Date()
+                let elapsed = end - start
+                assert.isAtLeast(elapsed, delay)
+                assert.equal(data, val)
+                done()
+            })
+    })
+    
     
 })
